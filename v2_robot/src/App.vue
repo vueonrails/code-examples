c09a895430cc23a3e65f1cf7b28d1b317e3fbeff
ytbryan <ytbryan@gmail.com>
2018-02-10 14:12:57 +0800

    Change the question for v2_robot

--- v2_robot/src/App.vue
+++ v2_robot/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-      <label>Search for:</label>
+      <label>Please ask a yes or no question. 
+        For example: Is the sky blue? </label>
       <input v-model="question">
       <button>Search</button>
       <p>{{ answer }}</p>
