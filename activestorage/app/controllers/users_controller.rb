8d524ed4a5d62d87743bae8f1195fd73ad60a4ff
Richard LaFranchi <rlafranchi@icloud.com>
2018-08-09 15:19:26 -0600

    [Chapter 4.4 ActiveStorage] initial w/ default file field

---
+++ activestorage/app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :avatar)
+  end
+end
