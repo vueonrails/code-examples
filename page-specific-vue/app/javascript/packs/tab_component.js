48c50c45a5cbcf76cc69e0f2ecc44bbb659220e9
ytbryan <ytbryan@gmail.com>
2018-08-20 22:43:54 +0800

    [Chapter 4.1 - Page Specific Vue] Use latest vuejs gem, use vue-on-rails at application.js, use isView at tab_component.js and use page_specific_vue helper at application.html.erb

--- page-specific-vue/app/javascript/packs/tab_component.js
+++ page-specific-vue/app/javascript/packs/tab_component.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-
 import App from '../parts/tab_component/tab_component.vue'
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(document.createElement('hello'))
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount('hello')
-  //console.log(app)
+  if(isView('pages#index')){
+    document.body.appendChild(document.createElement('hello'))
+    const app = new Vue({
+      render: h => h(App)
+    }).$mount('hello')
+    console.log({app})
+  }
 })
